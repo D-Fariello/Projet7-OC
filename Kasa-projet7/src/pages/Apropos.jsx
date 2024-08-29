67b148f1e72039c069bc09eb08e69b1f27ab7bb8
Dalila <dalila.fariello@gmail.com>
2024-08-29 21:07:36 +0200

    style: added dropdown transition effects and adjusted margins

--- Kasa-projet7/src/pages/Apropos.jsx
+++ Kasa-projet7/src/pages/Apropos.jsx
@@ -6,15 +6,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Apropos = () => {
-    // Use an object to track the open state of each dropdown
-    const [openDropdowns, setOpenDropdowns] = useState({});
+  // Use an object to track the open state of each dropdown
+  const [openDropdowns, setOpenDropdowns] = useState({});
 
-    const toggleDropdown = (index) => {
-      setOpenDropdowns((prevState) => ({
-        ...prevState,
-        [index]: !prevState[index]
-      }));
-    };
+  const toggleDropdown = (index) => {
+    setOpenDropdowns((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
 
   return (
     <>
@@ -24,7 +24,7 @@ const Apropos = () => {
           <IntroCard />
         </div>
         <ul className="a-propos-cards-container">
-          <li className="a-propos-cards">
+          <li className={`a-propos-cards ${openDropdowns[0] ? "open" : ""}`}>
             <h3>Fiabilité </h3>
             <FontAwesomeIcon
               icon={faChevronUp}
@@ -33,43 +33,67 @@ const Apropos = () => {
             />
             {openDropdowns[0] && (
               <div className="dropdown-content">
-                <p>Details about Fiabilité...</p>
+                <p>
+                  Les annonces postées sur Kasa garantissent une fiabilité
+                  totale. Les photos sont conformes aux logements, et toutes les
+                  informations sont régulièrement vérifiées par nos équipes.
+                </p>
               </div>
             )}
           </li>
-          <li className="a-propos-cards">
+          <li className={`a-propos-cards ${openDropdowns[1] ? "open" : ""}`}>
             <h3>Respect</h3>
-            <FontAwesomeIcon    icon={faChevronUp}
+            <FontAwesomeIcon
+              icon={faChevronUp}
               className={`cheveron-up ${openDropdowns[1] ? "open" : ""}`}
               onClick={() => toggleDropdown(1)}
             />
             {openDropdowns[1] && (
               <div className="dropdown-content">
-                <p>Details about Fiabilité...</p>
+                <p>
+                  La bienveillance fait partie des valeurs fondatrices de Kasa.
+                  Tout comportement discriminatoire ou de perturbation du
+                  voisinage entraînera une exclusion de notre plateforme.
+                </p>
               </div>
             )}
           </li>
-          <li className="a-propos-cards">
+          <li className={`a-propos-cards ${openDropdowns[2] ? "open" : ""}`}>
             <h3>Service</h3>
-            <FontAwesomeIcon    icon={faChevronUp}
+            <FontAwesomeIcon
+              icon={faChevronUp}
               className={`cheveron-up-2 ${openDropdowns[2] ? "open" : ""}`}
               onClick={() => toggleDropdown(2)}
             />
             {openDropdowns[2] && (
               <div className="dropdown-content">
-                <p>Details about Fiabilité...</p>
+                <p>
+                  La qualité du service est au cœur de notre engagement chez
+                  Kasa. Nous veillons à ce que chaque interaction, que ce soit
+                  avec nos hôtes ou nos locataires, soit empreinte de respect et
+                  de bienveillance.
+                </p>
               </div>
             )}
           </li>
-          <li className="a-propos-cards">
+          <li className={`a-propos-cards ${openDropdowns[3] ? "open" : ""}`}>
             <h3>Sécurité</h3>
-            <FontAwesomeIcon    icon={faChevronUp}
+            <FontAwesomeIcon
+              icon={faChevronUp}
               className={`cheveron-up ${openDropdowns[3] ? "open" : ""}`}
               onClick={() => toggleDropdown(3)}
             />
             {openDropdowns[3] && (
               <div className="dropdown-content">
-                <p>Details about Fiabilité...</p>
+                <p>
+                  La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes
+                  que pour les voyageurs, chaque logement correspond aux
+                  critères de sécurité établis par nos services. En laissant une
+                  note aussi bien à l'hôte qu'au locataire, cela permet à nos
+                  équipes de vérifier que les standards sont bien respectés.
+                  Nous organisons également des ateliers sur la sécurité
+                  domestique pour nos hôtes.
+                </p>
               </div>
             )}
           </li>
