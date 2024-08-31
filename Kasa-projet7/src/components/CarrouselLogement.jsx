4a02e1ed71cf0261ff8d365a48b6591a126a088d
Dalila <dalila.fariello@gmail.com>
2024-08-31 15:54:19 +0200

    style: adding style to the carrousel banner

--- Kasa-projet7/src/components/CarrouselLogement.jsx
+++ Kasa-projet7/src/components/CarrouselLogement.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CarrouselLogement = ({data}) => {
+const CarrouselLogement = ({ data }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-      return (
-        <div className="carrousel-images-container">
-          <img className="carrousel-images" src={data.pictures} alt="Logements" />
-          <div className="div-arrows">
-              <img className="arrow-left" src="/images/arrow-left.png" alt="fleche goche" />
-              <img className="arrow-right" src="/images/arrow-right.png" alt="fleche droite"/>
-          </div>
-        </div>
+  console.log("Data received:", data);
+  console.log("Pictures array:", data.pictures);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === data.pictures.length - 1 ? 0 : prevIndex + 1
     );
   };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? data.pictures.length - 1 : prevIndex - 1
+    );
+  };
+
+  if (!data.pictures || data.pictures.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="carrousel-images-container">
+      <img
+        className="carrousel-images"
+        src={data.pictures[currentIndex]}
+        alt="Logements"
+      />
+      <div className="div-arrows">
+        <img
+          className="arrow-left"
+          src="/images/arrow-left.png"
+          alt="fleche goche"
+          onClick={prevSlide}
+        />
+        <img
+          className="arrow-right"
+          src="/images/arrow-right.png"
+          alt="fleche droite"
+          onClick={nextSlide}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default CarrouselLogement;
--- Kasa-projet7/src/pages/Logement.jsx
+++ Kasa-projet7/src/pages/Logement.jsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from "react";
-import CarrouselLogement from '../components/CarrouselLogement';
+import CarrouselLogement from "../components/CarrouselLogement";
 
 const Logement = () => {
+  const [carrouselPictures, setCarrouselPictures] = useState([]);
 
-    const [carrouselPictures, setCarrouselPictures] = useState([]);
+  useEffect(() => {
+    // Fetch data from the JSON file
+    fetch("../data.json")
+      .then((response) => response.json())
+      .then((data) => setCarrouselPictures(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-    useEffect(() => {
-        // Fetch data from the JSON file
-        fetch("../data.json")
-          .then((response) => response.json())
-          .then((data) => setCarrouselPictures(data))
-          .catch((error) => console.error("Error fetching data:", error));
-      }, []);
-
-    return (
-        <main>
-            <div className="carrousel-container">
-            {carrouselPictures.map((pictures, index) => (
-                <CarrouselLogement key={index} data={pictures} /> 
-            ))}
-                
-            </div>
-        </main>
-    );
+  return (
+    <main>
+        {carrouselPictures.map((pictures, index) => (
+          <CarrouselLogement
+            key={index}
+            data={{pictures: pictures.pictures}}
+          />
+        ))}
+    </main>
+  );
 };
 
 export default Logement;
